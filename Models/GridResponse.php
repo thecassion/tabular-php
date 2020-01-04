066c9aa9922851c533323c5b55fe8cbb3893777a
thecassion <pierrerobentz.cassion@gmail.com>
2020-01-04 01:19:21 -0500

    Adding the first class

---
+++ Models/GridResponse.php
@@ -0,0 +1,14 @@
+<?php
+namespace tabularPhp\Model;
+
+class GridResponse {
+    private $aggregationPayload;
+    private $counter;
+    private $currentPage =0;
+    private $filteredRecordCount;
+    private $payload;
+    private $totalPages=1;
+    private $totalRecordCount;
+
+}
+?>
